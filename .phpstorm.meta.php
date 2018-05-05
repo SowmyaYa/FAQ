53cc33c650455bef402e819da39dac232c67172d
sowmya <sy379@njit.edu>
2018-05-05 16:42:15 -0400

    Dusk installed. Browser folder is created. Performed a test to see if the title of my project is FAQ and test successful

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-05-03 03:59:35.
+    * Generated on 2018-05-05 20:29:28.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
